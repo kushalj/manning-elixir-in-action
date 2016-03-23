5f22cfcbd017f0b68a580f610148b6dc1d292eb8
Kushal <kushal@elysian.net>
2016-03-23 20:11:52 +0000

    recursive do_sum added

--- sum_list.ex
+++ sum_list.ex
@@ -1,7 +1,11 @@
 defmodule ListHelper do
-	def sum([]), do: 0
-	def sum([head | tail]) do
-	  head + sum(tail)
+	def sum(list) do
+		do_sum(0, list)
+	end
+
+	def do_sum(total, []), do: total 
+	def do_sum(total, [head | tail]) do
+	  do_sum(head+total, tail)
 	end
 end
 
